b11d5d66624139bdfe5366b22799874e13b32a31
atul <atkr5496@gmail.com>
2025-08-15 23:08:18 +0530

    multi-module setup

---
+++ booking-service/src/test/java/com/proj/bookingservice/BookingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proj.bookingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
